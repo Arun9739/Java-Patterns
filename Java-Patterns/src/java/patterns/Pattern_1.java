2e58de0765db8f6ca65b96bbd9cbbf19251b23e3
Arun G Nayak <nayakarun02@gmail.com>
2022-08-02 22:06:39 +0530

    pattern 1 added

---
+++ Java-Patterns/src/java/patterns/Pattern_1.java
@@ -0,0 +1,22 @@
+
+/* A Program on generating the pattern of series of numbers from 1 to n */
+
+//package java.patterns;
+
+import java.util.Scanner;
+
+public class Pattern_1 {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Enter the number of elments in a pattern : ");
+        int n = scan.nextInt();
+        
+        for(int i = 1; i <= n; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    
+}
