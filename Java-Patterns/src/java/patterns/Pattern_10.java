807844d7bc45a40cee4f6c3ebaf7289b5a046b55
Arun G Nayak <nayakarun02@gmail.com>
2022-12-06 21:23:40 +0530

    Create Pattern_10.java

---
+++ Java-Patterns/src/java/patterns/Pattern_10.java
@@ -0,0 +1,22 @@
+
+/* This program prints * in some increasing fashion 'n' times */
+
+import java.util.Scanner;
+
+public class Pattern_10 {
+    public static void main(String[] args) {
+        int n;
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Enter number of rows : ");
+        n = scan.nextInt();
+        
+        for(int i =0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
