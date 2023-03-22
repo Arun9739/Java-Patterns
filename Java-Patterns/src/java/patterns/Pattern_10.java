ade21526970b1fe990081a169f953b09b00a22e6
Arun G Nayak <nayakarun02@gmail.com>
2023-03-22 20:24:21 +0530

    Update Pattern_10.java

--- Java-Patterns/src/java/patterns/Pattern_10.java
+++ Java-Patterns/src/java/patterns/Pattern_10.java
@@ -11,7 +11,7 @@ public class Pattern_10 {
         System.out.println("Enter number of rows : ");
         n = scan.nextInt();
         
-        for(int i =0; i < n; i++){
+        for(int i = 1; i <= n; i++){
             for(int j = 0; j < i; j++){
                 System.out.print("* ");
             }
